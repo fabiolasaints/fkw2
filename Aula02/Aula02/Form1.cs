9d5f0966a4f234e3178a6759751a080f25e8dfa6
Kyrla <kyrlang@bol.com.br>
2019-02-14 21:36:19 -0200

    primeiro commit kyrla

--- Aula02/Aula02/Form1.cs
+++ Aula02/Aula02/Form1.cs
@@ -33,6 +33,70 @@ namespace Aula02
                           .Select(s => s[random.Next(s.Length)])
                           .ToArray());
 
+
+
+            //Straight
+            List<string> cartasMao1 = new List<string>();
+            cartasMao1.Add(mao1.Substring(0, 1));
+            cartasMao1.Add(mao1.Substring(1, 1));
+            cartasMao1.Add(mao1.Substring(2, 1));
+            cartasMao1.Add(mao1.Substring(3, 1));
+            cartasMao1.Add(mao1.Substring(4, 1));
+
+            cartasMao1.OrderBy(c => c).ToList();
+            List<string> mao1Aux = new List<string>();
+
+            foreach (var item in cartasMao1)
+            {
+
+                switch (item)
+                {
+                    case "J":
+                        mao1Aux.Add("11");
+                        break;
+                    case "Q":
+                        mao1Aux.Add("12");
+                        break;
+                    case "K":
+                        mao1Aux.Add("13");
+                        break;
+                    case "A":
+                        mao1Aux.Add("14");
+                        break;
+                    default:
+                        mao1Aux.Add(item);
+                        break;
+                }
+
+            }
+
+            cartasMao1 = mao1Aux;
+            cartasMao1.OrderBy(c => c).ToList();
+
+            foreach (var item in cartasMao1)
+            {
+                int seq = 0;
+                int controle = 0;
+                string sequencia = string.Empty;
+
+                if (sequencia.Length == 0)
+                {
+                    controle = seq;
+                    sequencia = sequencia + item.ToString();
+                }
+                else
+                {
+                    if (controle + 1 == seq)
+                        sequencia = sequencia + item.ToString();
+                }
+
+                if (sequencia.Length == 5)
+                    MessageBox.Show("Straight" + Environment.NewLine + sequencia);
+                else
+                    MessageBox.Show("Não rolou Straight" + Environment.NewLine + mao1.ToString());
+
+            }
+
         }
     }
 }
